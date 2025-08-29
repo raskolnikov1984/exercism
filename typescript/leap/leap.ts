147528f0e39777f291bcf4599efd89408fb7cf04
rodia <alejandro.ayala0@gmail.com>
2025-08-28 22:59:29 -0300

    refactor: leap

--- typescript/leap/leap.ts
+++ typescript/leap/leap.ts
@@ -1,8 +1,3 @@
 export function isLeap(year: number) {
-  if (year % 4 === 0 && year % 400 === 0) {
-    return true;
-  } else if (year % 4 === 0 && year % 100 !== 0) {
-    return true;
-  }
-  return false;
+  return (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0));
 }
